5863b1e34aa344373a44a8400060e0ed6791c0f5
Matt Ryan <44824894+mattryanmtl@users.noreply.github.com>
2021-02-22 10:25:41 -0500

    Create DayTwentythree.py

---
+++ Python/DayTwentythree.py
@@ -0,0 +1,14 @@
+def ducci_sequence(*ns):
+    while True:
+        yield ns
+        ns = tuple(abs(ns[i - 1] - ns[i]) for i in range(len(ns)))
+
+def ducci(*ns):
+    known = set()
+    for ns in ducci_sequence(*ns):
+        print(ns)
+        if ns in known or set(ns) == {0}:
+            break
+        known.add(ns)
+    return len(known) + 1
+print(ducci(0, 345, 1788, 5078), "steps")
