ac3d252e57822591128482ca424d6caacd799c34
Matt Ryan <44824894+mattryanmtl@users.noreply.github.com>
2021-03-14 21:25:42 -0400

    Create DayFortyone.js

---
+++ Javascript/DayFortyone.js
@@ -0,0 +1,15 @@
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+}
